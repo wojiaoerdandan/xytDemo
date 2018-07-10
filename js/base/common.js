0bedb9abcaca1552a930d4fe714c81958fc255fc
gyp <15901224483@163.com>
2018-07-10 14:11:59 +0800

    更改了README.md文件

--- js/base/common.js
+++ js/base/common.js
@@ -1,11 +1,12 @@
 $(function(){
+	$("body").append("<div class='ReturnTop back-top'><a href='javascript:void(0)' target='_blank'></a><a target='_blank' class='js-back-top' href='javascript:void(0)'></a></div>")
     /*返回顶部 只有距离顶部的距离大于400像素时才显示返回顶部*/
 	$(window).scroll(function(){
 		var top = $(window).scrollTop();
 		if(top>300){
-			$(".back-top").show();
+			$(".back-top").fadeIn();
 		}else{
-			$(".back-top").hide();
+			$(".back-top").fadeOut();
 		}
 	});
 	/*返回顶部*/
